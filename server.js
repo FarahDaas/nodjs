1c85069185f3639a81f4b9af3648b649638afd06
FarahDaas <farahdaas@outlook.fr>
2022-02-19 16:12:45 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http')
+
+
+const server = http.createServer((req,res)=>{
+    
+    if(req.url=='/home'){
+        res.statusCode=200
+        res.write('welcom from home')
+    }else if(req.url=='/contact'){
+        res.statusCode=200
+        res.write('welcom from contact')
+    }else if(req.url=='/about'){
+        res.statusCode=200
+        res.write('welcom from about')
+    }else{
+        res.statusCode=404
+        res.write('not found')
+    }
+    
+    res.end()
+    
+})
+server.listen(3000,()=>console.log('server running'))
